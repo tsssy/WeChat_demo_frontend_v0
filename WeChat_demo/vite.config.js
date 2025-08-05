2523bbae1db262a43a546eefb2e57cf8f8c9e899
tsssy <btusaya491@163.com>
2025-08-05 19:31:47 +0800

    更新后端url，添加抽卡游戏调用指南

--- WeChat_demo/vite.config.js
+++ WeChat_demo/vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0', // 允许外部访问
+    port: 5173, // 指定端口
     proxy: {
       '/api': {
         target: 'https://loveluretech.xyz',
